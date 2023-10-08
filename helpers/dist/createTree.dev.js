fbae7daa69ac04cf7c2e7253292a71952a167df1
chisu15 <trunghieudo1509@gmail.com>
2023-10-08 16:31:02 +0700

    Sửa lỗi số thứ tự

--- helpers/dist/createTree.dev.js
+++ helpers/dist/createTree.dev.js
@@ -1,11 +1,15 @@
 "use strict";
 
+var count = 0;
+
 var createTree = function createTree(arr) {
   var parentId = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "";
   var tree = [];
   arr.forEach(function (item) {
     if (item.parent_id === parentId) {
+      count++;
       var newItem = item;
+      newItem.index = count;
       var children = createTree(arr, item.id);
 
       if (children.length > 0) {
@@ -20,6 +24,7 @@ var createTree = function createTree(arr) {
 
 module.exports.tree = function (arr) {
   var parentId = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "";
+  count = 0;
   var tree = createTree(arr, parentId = "");
   return tree;
 };
