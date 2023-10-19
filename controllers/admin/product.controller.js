3be0a766aad9b2b457ca62738caf52b4138fb408
chisu15 <trunghieudo1509@gmail.com>
2023-10-19 11:43:27 +0700

    Log sản phẩm - xóa

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -103,7 +103,10 @@ module.exports.changeMulti = async (req, res) => {
         { _id: { $in: ids } },
         {
           deleted: true,
-          deletedAt: new Date(),
+          deletedBy: {
+            account_id: res.locals.user.id,
+            deletedAt: new Date(),
+          }
         }
       );
       req.flash("success", `Đã xóa thành công ${ids.length} sản phẩm!`);
@@ -142,7 +145,10 @@ module.exports.deleteItem = async (req, res) => {
     { _id: id },
     {
       deleted: true,
-      deletedAt: new Date(),
+      deletedBy: {
+        account_id: res.locals.user.id,
+        deletedAt: new Date(),
+      }
     }
   );
 
